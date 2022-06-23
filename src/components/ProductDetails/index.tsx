30bb66af29b157208132f3e57570de6ffc8b8d93
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2022-06-22 23:33:27 -0700

    style: update product page style

--- src/components/ProductDetails/index.tsx
+++ src/components/ProductDetails/index.tsx
@@ -9,10 +9,14 @@ const Product = (props: ProductProps) => {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-      <div>
-        <img src={product.image} alt={product.title} />
+      <div className="bg-white rounded-md p-4">
+        <img
+          src={product.image}
+          alt={product.title}
+          className="max-h-[420px] mx-auto"
+        />
       </div>
-      <div>
+      <div className="bg-white rounded-md p-6">
         <h1 className="text-xl font-semibold">{product.title}</h1>
         <p className="text-base text-gray-600 my-6">{product.description}</p>
         <span className="block text-2xl font-semibold">${product.price}</span>
